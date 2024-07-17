ad50b3bef14f332cb7ce19a8c3778381092ba721
Abhinav Raj Srivastava <abhiraj.srivast254@gmail.com>
2024-07-18 01:17:39 +0530

    18-07-2024-001

--- File_Sys/SDT/Serial.java
+++ File_Sys/SDT/Serial.java
@@ -22,18 +22,25 @@ public class Serial {
     public static void main(String[] args) {
         Arun arun = new Arun("Arun", 21, "Bangalore");
 
+        //creating a directory
         File directory = new File("Data");
+        //checking if the directory exists - if not, create it
         directory.mkdir(); 
-
+        
+        //creating a file, name it Arun.ser inside the directory Data
         File file = new File(directory, "Arun.ser");
         try {
+            //checking if the file exists - if not, create it
             file.createNewFile(); 
-
+            
+            //this creates a file output stream to write to the file
             FileOutputStream fos = new FileOutputStream(file);
-
+ 
             //this serializes the object
+            //ObjectOutputStream is used to write primitive data types and Java objects to an OutputStream
             ObjectOutputStream oos = new ObjectOutputStream(fos);
-
+            
+            //write the object to the file
             oos.writeObject(arun);
             oos.close();
             fos.close();
